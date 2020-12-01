9949662652beb6f307a9101a1e7eeff70b4a33aa
rvanbaalen <rvanbaalen@users.noreply.github.com>
2020-12-01 17:33:16 +0000

    compiled package files

--- dist/hashparser.js
+++ dist/hashparser.js
@@ -1,2 +1,5 @@
-"use strict";function hashParserReg(a){return new RegExp("(#!|&)("+a+"=)(.[^&]*)")}window.HashParser={setParameter:function setParameter(a,b,c){var d=location.hash;if(c=!!c,b=c?btoa(JSON.stringify(b)):encodeURIComponent(b),-1<d.indexOf(a))location.hash=d.replace(hashParserReg(a),"$1$2"+b);else{// Add the param
+"use strict";/**
+ * @param key
+ * @returns {RegExp}
+ */function hashParserReg(a){return new RegExp("(#!|&)("+a+"=)(.[^&]*)")}window.HashParser={setParameter:function setParameter(a,b,c){var d=location.hash;if(c=!!c,b=c?btoa(JSON.stringify(b)):encodeURIComponent(b),-1<d.indexOf(a))location.hash=d.replace(hashParserReg(a),"$1$2"+b);else{// Add the param
 var e="#!"===d.substr(0,2)?"&":"#!";location.hash=d+e+a+"="+b}},getParameter:function getParameter(a,b){var c=location.hash.match(hashParserReg(a));if(b=!!b,null!==c)c=c[c.length-1];else return c;return b?JSON.parse(atob(c)):decodeURIComponent(c)}};
