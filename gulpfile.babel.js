a497e0377b83630321d895dfb4a79d30be52fad6
rvanbaalen <robin@ganttify.com>
2020-12-02 11:44:59 -0400

    feat: add minified code
    Signed-off-by: rvanbaalen <robin@ganttify.com>

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,10 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import minify from 'gulp-minify';
 
 exports.default = () => {
     return gulp.src('./src/hashparser.js')
         .pipe(babel())
+        .pipe(minify())
         .pipe(gulp.dest('./dist'));
 };
