3fdc2bec10938f0d890d819df64624a2c7560b47
Robin van Baalen <robin@ganttify.com>
2026-03-28 22:41:47 -0400

    fix: use HTTPS repository URL to fix broken GitHub links on project page
    The readme-to-html tool generates GitHub links from the repository.url
    field in package.json. The git+ssh:// URL format produced malformed
    HTTPS links (https://git@github.com/...). Switching to the HTTPS URL
    format fixes the "View on GitHub" and "Repository" footer links.

--- rtoh.config.js
+++ rtoh.config.js
@@ -11,6 +11,5 @@ export default {
     "{{OG_IMAGE_HEIGHT}}": "640",
     "{{FULL_URL}}": "https://hashparser.robinvanbaalen.nl/",
     "https://socialify.git.ci/rvanbaalen/readme-to-html/image?language=1&name=1&owner=1&pattern=Charlie+Brown&theme=Light": "https://socialify.git.ci/rvanbaalen/hashparser/image?language=1&name=1&owner=1&pattern=Charlie+Brown&theme=Light",
-    "https://git@github.com/rvanbaalen/hashparser": "https://github.com/rvanbaalen/hashparser",
   },
 }
